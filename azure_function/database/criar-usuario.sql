34196b9251b268c816141f410f3385c9cbae577d
Modesto <joao.modesto12373@gmail.com>
2026-01-27 19:26:19 -0300

    correcao de bug

--- azure_function/database/criar-usuario.sql
+++ azure_function/database/criar-usuario.sql
@@ -1,7 +1,14 @@
 -- Cria um usuário no banco vinculado à sua Function
-CREATE USER [app-function] FROM EXTERNAL PROVIDER;
+CREATE USER [name_app] FROM EXTERNAL PROVIDER;
 
 -- Dá permissão de leitura, escrita e execução (necessário para atualizar KPIs)
-ALTER ROLE db_datareader ADD MEMBER [app-function];
-ALTER ROLE db_datawriter ADD MEMBER [app-function];
-ALTER ROLE db_ddladmin ADD MEMBER [app-function]; -- Se o seu script criar tabelas
+ALTER ROLE db_datareader ADD MEMBER [name_app];
+ALTER ROLE db_datawriter ADD MEMBER [name_app];
+ALTER ROLE db_ddladmin ADD MEMBER [name_app]; -- Se o seu script criar tabelas
+
+-- Concede as permissões necessárias para ler e gravar dados
+ALTER ROLE db_datareader ADD MEMBER [name_app];
+ALTER ROLE db_datawriter ADD MEMBER [name_app];
+
+-- Para executar scripts DDL 
+ALTER ROLE db_ddladmin ADD MEMBER [name_app];
